594282800bb85900f8dfa5694a8e979ebd467896
Kevin Cooper <boo2atfoo@gmail.com>
2013-10-23 01:00:09 -0600

    Adding the source code provided.  I am not too sure if I should be using the header file demonstrated in the online document or if I should somehow come up with my own somehow without the previous knowledge of seeing his header file.  I will use Capt. Branchflowers header implementation until further clarification

--- LCDDriver.c
+++ LCDDriver.c
@@ -6,3 +6,100 @@
  */
 
 
+#define RS_MASK 0x40
+
+/*
+ * Author: Todd Branchflower
+ */
+void LCDinit()
+{
+    writeCommandNibble(0x03);
+
+    writeCommandNibble(0x03);
+
+    writeCommandNibble(0x03);
+
+    writeCommandNibble(0x02);
+
+    writeCommandByte(0x28);
+
+    writeCommandByte(0x0C);
+
+    writeCommandByte(0x01);
+
+    writeCommandByte(0x06);
+
+    writeCommandByte(0x01);
+
+    writeCommandByte(0x02);
+
+    SPI_send(0);
+    delayMicro();
+}
+/*
+ * Author: Todd Branchflower
+ */
+void writeCommandNibble(char commandNibble)
+{
+    LCDCON &= ~RS_MASK;
+    LCD_write_4(commandNibble);
+    delayMilli();
+}
+/*
+ * Author: Todd Branchflower
+ */
+void writeCommandByte(char commandByte)
+{
+    LCDCON &= ~RS_MASK;
+    LCD_write_8(commandByte);
+    delayMilli();
+}
+/*
+ * Author: Todd Branchflower
+ */
+void writeDataByte(char dataByte)
+{
+    LCDCON |= RS_MASK;
+    LCD_write_8(dataByte);
+    delayMilli();
+}
+/*
+ * Author: Todd Branchflower
+ */
+void LCD_write_8(char byteToSend)
+{
+    unsigned char sendByte = byteToSend;
+
+    sendByte &= 0xF0;
+
+    sendByte = sendByte >> 4;               // rotate to the right 4 times
+
+    LCD_write_4(sendByte);
+
+    sendByte = byteToSend;
+
+    sendByte &= 0x0F;
+
+    LCD_write_4(sendByte);
+}
+/*
+ * Author: Todd Branchflower
+ */
+void SPI_send(char byteToSend)
+{
+    char readByte;
+
+    set_SS_lo();
+
+    UCB0TXBUF = byteToSend;
+
+    while(!(UCB0RXIFG & IFG2))
+    {
+        // wait until you've received a byte
+    }
+
+    readByte = UCB0RXBUF;
+
+    set_SS_hi();
+}
+
--- LCDDriver.h
+++ LCDDriver.h
@@ -1,13 +1,27 @@
-/*
- * LCDDriver.h
- *
- *  Created on: Oct 23, 2013
- *      Author: C15Kevin.Cooper
- */
+//
+// lcd.h
+// Author: Todd Branchflower
+// Date: 17 October 2013
+// Description: Library to interact with the Geek Box LCD.
+//
 
 #ifndef LCDDRIVER_H_
 #define LCDDRIVER_H_
 
+void initSPI();
 
+void LCDinit();
+
+void LCDclear();
+
+void cursorToLineTwo();
+
+void cursorToLineOne();
+
+void writeChar(char asciiChar);
+
+void writeString(char * string);
+
+void scrollString(char * string1, char * string2);
 
 #endif /* LCDDRIVER_H_ */
