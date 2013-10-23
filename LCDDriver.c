4e50a487e2e08db6be3afdfaadfe345454aa9116
Kevin Cooper <boo2atfoo@gmail.com>
2013-10-23 00:55:53 -0600

    Creation of the two LCD driver files, header and source.

---
+++ LCDDriver.c
@@ -0,0 +1,8 @@
+/*
+ * LCDDriver.c
+ *
+ *  Created on: Oct 23, 2013
+ *      Author: C15Kevin.Cooper
+ */
+
+
---
+++ LCDDriver.h
@@ -0,0 +1,13 @@
+/*
+ * LCDDriver.h
+ *
+ *  Created on: Oct 23, 2013
+ *      Author: C15Kevin.Cooper
+ */
+
+#ifndef LCDDRIVER_H_
+#define LCDDRIVER_H_
+
+
+
+#endif /* LCDDRIVER_H_ */
